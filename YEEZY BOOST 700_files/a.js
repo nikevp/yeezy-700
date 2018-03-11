3539762ea3654e4ba5a87de34ca0e3ebe8dadd48
nikevp <kasper.ad@husky.neu.edu>
2018-03-11 02:23:20 +0000

    Add files via upload

---
+++ YEEZY BOOST 700_files/a.js
@@ -0,0 +1,9 @@
+/* URL: https://4599367.fls.doubleclick.net/activityi;dc_pre=CJumtKOU4tkCFZBRhgodSrUBuw;src=4599367;type=products;cat=prddtl;u1=PRODUCT%7CYEEZY%20BOOST%20700%20(B75571);u3=;u4=;u7=BSY42;u9=ADIDAS;u11=0;u12=0;u16=SHOES;u17=BSY42;u18=300.00;u19=B75571;u20=W4XT62X621KAIF34;u23=;u25=TEST;u28=YEEZY%20BOOST%20700;u26=300.00;ord=1;num=1310304431004? */
+/* Extended Data: ab_test_id = W4XT62X621KAIF34, is_custom = , mobile_items = 0, mobile_transactions = 0, order_total = , page_name = PRODUCT%7CYEEZY%20BOOST%20700%20(B75571), product_brand = ADIDAS, product_category_list = SHOES, product_colour_list = , product_gender = , product_group = , product_id_list = B75571, product_model_id = BSY42, product_model_id_list = BSY42, product_price_list = 300.00, product_quantitiy = , product_size_list = , product_sport = , tag = 11237203604141, total_products = , voucher_code =  */
+/* Actions Fired:
+* Id = 11237203604141, Name = Performance - Product Details Page - US
+*/
+/* Third-Party Tags Fired:
+None
+*/
+!function(){function e(e){function t(e){for(var a=e.childNodes,o=0;o<a.length;o++)t(a[o]);if(e.tagName===n){var c=document.createElement(n);e.getAttribute(r)&&c.setAttribute(r,e.getAttribute(r)),c.text=e.text,e.parentNode.insertBefore(c,e),e.parentNode.removeChild(e)}}var n="SCRIPT",r="src",a=document.createDocumentFragment(),o=document.createElement("I");a.appendChild(o),o.innerHTML='<em style="display:none;">&nbsp;</em>'+e,t(o);var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(a,c)}var t="";if(t){var n=document.write;document.write=function(t){try{n(t)}catch(r){e(t)}},e(t)}}();
